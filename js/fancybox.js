314183494828a844257a8f3a7b210723a578409e
Setiadyyy <m.setiadysaputra14@gmail.com>
2024-03-23 11:39:57 +0700

    menambahkan people card

---
+++ js/fancybox.js
@@ -0,0 +1,17 @@
+Fancybox.bind('[data-fancybox="gallery"]', {
+    toolbar: {
+      display: {
+        left: ["infobar"],
+        middle: [
+          "zoomIN",
+          "zoomOut",
+          "toggle1to1",
+          "rotateCCW",
+          "rotateCW",
+          "flipX",
+          "flipY",
+        ],
+        right: ["slideshow", "thumbs", "close"],
+      },
+    },
+  });
